862c19cf3c745908b62147b0a1f5f14659898109
Jaosuzart <joaomarcelosuzartcastro@gmail.com>
2026-08-09 14:31:44 -0300

    Fix api/game/ranking auth middleware

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -4,16 +4,14 @@ const gameController = require('../controllers/gameController');
 const { body, param } = require('express-validator');
 const { tratarErrosValidacao } = require('../middlewares/validators');
 
-router.use(gameController.validarApiKey);
-
 router.get('/saldo/:email', [
   param('email').isEmail().withMessage('O e-mail fornecido é inválido.')
-], tratarErrosValidacao, gameController.getSaldo);
+], tratarErrosValidacao, gameController.validarApiKey, gameController.getSaldo);
 
 router.post('/consumir', [
   body('email').isEmail().withMessage('Um e-mail válido é obrigatório.'),
   body('quantidade').isInt({ min: 1 }).withMessage('A quantidade deve ser um número inteiro maior que 0.')
-], tratarErrosValidacao, gameController.consumirTokens);
+], tratarErrosValidacao, gameController.validarApiKey, gameController.consumirTokens);
 
 router.get('/ranking', gameController.getRanking);
 
@@ -22,6 +20,6 @@ router.post('/stats', [
   body('trofeus').optional().isInt(),
   body('vitorias').optional().isInt(),
   body('xp').optional().isInt()
-], tratarErrosValidacao, gameController.salvarEstatisticas);
+], tratarErrosValidacao, gameController.validarApiKey, gameController.salvarEstatisticas);
 
 module.exports = router;
